05405b4b11d99a60c15e0bbf4bf83c9a9cd4c3d1
plusdojo2026-okawa <okawa-ryotaro-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-15 17:57:57 +0900

    Update MamanoyousuDao.java

--- source/src/main/java/dao/MamanoyousuDao.java
+++ source/src/main/java/dao/MamanoyousuDao.java
@@ -1,5 +1,10 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 import dto.Mamanoyousu;
 
 public class MamanoyousuDao {
@@ -14,6 +19,53 @@ public class MamanoyousuDao {
     	String sql="INSERT INTO mamanoyousu "
     	                + "(family_id, couple_id, physical_score, mental_score, sleep_hours, stress, memo, recorded_date) "
     	                + "VALUES (?, ?, ?, ?, ?, ?, ?, NOW())";
+    	try(Connection conn= 
+    			DriverManager.getConnection(URL, USER, PASS);
+                PreparedStatement ps = conn.prepareStatement(sql)){
+ 
 
+    		ps.setString(1, m.getFamilyId());
+            ps.setInt(2, m.getCoupleId());
+            ps.setInt(3, m.getPhysicalScore());
+            ps.setInt(4, m.getMentalScore());
+            ps.setDouble(5, m.getSleepHours());
+            ps.setInt(6, m.getStress());
+            ps.setString(7, m.getMemo());
+
+            return ps.executeUpdate() > 0;
+
+    }catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
-}
+
+// 最新データ取得
+     public Mamanoyousu findLatest(String familyId) {
+
+         String sql = "SELECT * FROM mamanoyousu "
+                    + "WHERE family_id=? ORDER BY recorded_date DESC LIMIT 1";
+
+         try (Connection conn = DriverManager.getConnection(URL, USER, PASS);
+              PreparedStatement ps = conn.prepareStatement(sql))
+         {    
+        	 ps.setString(1, familyId);
+             ResultSet rs = ps.executeQuery();
+
+             if (rs.next()) {
+                 Mamanoyousu m = new Mamanoyousu();
+                 m.setPhysicalScore(rs.getInt("physical_score"));
+                 m.setMentalScore(rs.getInt("mental_score"));
+                 m.setSleepHours(rs.getDouble("sleep_hours"));
+                 m.setStress(rs.getInt("stress"));
+                 m.setMemo(rs.getString("memo"));
+                 return m;
+             }
+
+         } catch (Exception e) {
+             e.printStackTrace();
+         }
+         return null;
+     }
+ }
+
