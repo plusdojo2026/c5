5ab0cd29762d91cd39b5babbc7e8802d4b9116cb
plusdojo2026-okawa <okawa-ryotaro-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-15 15:25:32 +0900

    Update MamanoyousuDao.java

--- source/src/main/java/dao/MamanoyousuDao.java
+++ source/src/main/java/dao/MamanoyousuDao.java
@@ -1,5 +1,19 @@
 package dao;
 
-public class MamanoyousuDao {
+import dto.Mamanoyousu;
 
+public class MamanoyousuDao {
+	//データベース接続
+	private static final String URL= "jdbc:mysql://localhost:3306/";//接続するデータベース名
+	private static final String USER     = "root";//MySQLにログインするユーザー名
+											      //⚠root は 管理者ユーザー
+    private static final String PASSWORD = "";//上記ユーザーのパスワード
+    
+    //データ登録
+    public boolean insert(Mamanoyousu m) {
+    	String sql="INSERT INTO mamanoyousu "
+    	                + "(family_id, couple_id, physical_score, mental_score, sleep_hours, stress, memo, recorded_date) "
+    	                + "VALUES (?, ?, ?, ?, ?, ?, ?, NOW())";
+
+    }
 }
