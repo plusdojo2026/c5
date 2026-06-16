be4fd3b984a23649c3cf58e76b557872089ac919
plusdojo2026-mukogawa <mukogawa-ryo-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-16 17:19:31 +0900

    Update OmoidealbumDao.java

--- source/src/main/java/dao/OmoidealbumDao.java
+++ source/src/main/java/dao/OmoidealbumDao.java
@@ -3,43 +3,132 @@ package dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+
 import dto.Omoidealbum;
 
 public class OmoidealbumDao {
-
-	public boolean insert(Omoidealbum album) {
+	
+	//--------日記の一覧をfamilyIdで取得し、タイムライン形式で並べる。--------
+		public List<Omoidealbum> FindByFamilyId(String familyId){
+		//結果を格納するリスト
+			List<Omoidealbum> list = new ArrayList<Omoidealbum>();
+		//DB接続に使う変数
+			Connection conn = null;
+		//JDBCドライバを読み込む
+			try {Class.forName("com.mysql.cj.jdbc.Driver");
+		//DBに接続
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/futarigoto_db?characterEncoding=UTF-8","root","");
+		//SQLを準備
+			String sql = "SELECT * FROM album WHERE family_id=? ORDER BY albumId DESC";
+			PreparedStatement ps = conn.prepareStatement(sql);
+		//SQLを完成
+			ps.setString(1,familyId);
+		// SQL文を実行し、結果表を取得する
+			ResultSet rs = ps.executeQuery();
+		// 結果表をコレクションにコピーする
+			while (rs.next()) {
+			Omoidealbum album = new Omoidealbum(rs.getString("family_id"),
+											 	rs.getInt("couple_id"),
+											 	rs.getInt("albumId"),
+											 	rs.getString("photo_path"), 
+											 	rs.getString("comment"),
+											 	rs.getTimestamp("created_at"));
+				list.add(album);
+			}
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+		// データベースを切断
+			if (conn != null)
+				try { conn.close();
+					} catch (SQLException e) {}
+					}
+		// 結果を返す
+		return list;
+		}
+	
+	//--------引数albumで指定されたレコードを削除し、成功したらtrueを返す--------
+		public boolean delete(Omoidealbum album) {
 		Connection conn = null;
 		boolean result = false;
 		
-		/*JDBCドライバ読み込み*/
 		try {
-			Class.forName("conn.mysql.cj.jdbc.Driver");
-		/*DBに接続*/
-			conn = DriverManager.getConnection("");
-		/*SQLを準備*/
+		//JDBCドライバ読み込み
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		//DBに接続
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/futarigoto_db?characterEncoding=UTF-8","root","");
+		//SQLを準備
+		String sql = "DELETE FROM album WHERE albumId=?";
+		PreparedStatement pStmt = conn.prepareStatement(sql);
+		//SQLを完成
+		pStmt.setInt(1,album.getAlbumId());
+		//SQLを実行
+		if(pStmt.executeUpdate()==1) {
+				result = true;
+		}
+	}catch(SQLException e) {
+		e.printStackTrace();
+	} catch (ClassNotFoundException e) {
+		e.printStackTrace();
+	} finally {
+		// データベースを切断
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	// 結果を返す
+	return result;
+}
+	
+	
+	//--------日記を登録する--------//
+		//引数albumで指定されたレコードを登録し、成功したらtrueを返す//
+		public boolean insert(Omoidealbum album) {
+		Connection conn = null;
+		boolean result = false;
+		
+		//JDBCドライバ読み込み
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		//DBに接続
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/futarigoto_db?characterEncoding=UTF-8","root","");
+		//SQLを準備
 		String sql = "INSERT INTO album(family_id,couple_id,photo_path,comment,created_at)"
 					+"VALUES(?,?,?,?,NOW())";
 		PreparedStatement ps = conn.prepareStatement(sql);
 		
-		/*SQLの？に値を入れる*/
+		//SQLの？に値を入れる
 		ps.setString(1, album.getFamilyId());
 		ps.setInt(2,album.getCoupleId());
-		ps.setString(3,album.getPhotoPath());
-		ps.setString(4, album.getComment());
+		ps.setInt(3,album.getAlbumId());
+		ps.setString(4,album.getPhotoPath());
+		ps.setString(5, album.getComment());
+		ps.setTimestamp(6,album.getCreatedAt());
 		
-		/*SQLを実行する*/
+		//SQLを実行
 		if(ps.executeUpdate()==1) {
 			result = true;
 		}
 		
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+		//DBを切断
+			if(conn != null)try {conn.close();}catch(SQLException e) {}
 		}
+		//結果を返す
+		return result;
 	}
 	
+	
 }
-
-
-
-更新
