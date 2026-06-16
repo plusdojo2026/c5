1dd7b8f05beff168ddbb4fac0f34c5f20998270d
plusdojo2026-nakajiataku <nakajima-taku-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-16 16:59:14 +0900

    Update futarinobalance.sql

--- source/sql/futarinobalance.sql
+++ source/sql/futarinobalance.sql
@@ -1,13 +1,16 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<classpath>
-	<classpathentry kind="con" path="org.eclipse.jdt.launching.JRE_CONTAINER/org.eclipse.jdt.internal.debug.ui.launcher.StandardVMType/JavaSE-17"/>
-	<classpathentry kind="src" path="src/main/java"/>
-	<classpathentry kind="con" path="org.eclipse.jst.server.core.container/org.eclipse.jst.server.tomcat.runtimeTarget/Tomcat9 (Java17)">
-		<attributes>
-			<attribute name="owner.project.facets" value="jst.web"/>
-		</attributes>
-	</classpathentry>
-	<classpathentry kind="con" path="org.eclipse.jst.j2ee.internal.web.container"/>
-	<classpathentry kind="con" path="org.eclipse.jst.j2ee.internal.module.container"/>
-	<classpathentry kind="output" path="build/classes"/>
-</classpath>
+-- ============================================================
+--  8. futarinobalance (ふたりのバランス)
+--     21個のタスクを「妻/夫/未割当」で振り分ける表
+--     ※ couple_id は 0=妻 / 1=夫 / 2=未割当 (FutarinobalanceDaoより)
+--     ※ couple_id=2 は users に存在しないので、ここでは複合FKは張らない
+-- ============================================================
+CREATE TABLE futarinobalance (
+    balance_id    INT         NOT NULL AUTO_INCREMENT,  -- バランスID (主キー)
+    family_id     VARCHAR(32) NOT NULL,                 -- ファミリーID
+    couple_id     INT         NOT NULL DEFAULT 2,       -- 担当 (0=妻, 1=夫, 2=未割当)
+    todo_id       INT         NOT NULL,                 -- FK → todo
+    display_order INT         NOT NULL DEFAULT 0,       -- 表示順
+    PRIMARY KEY (balance_id),
+    FOREIGN KEY (todo_id) REFERENCES todo (task_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
