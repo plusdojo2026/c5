daa30cc3fd19dad7679d18534b7207871b0fc0a9
plusdojo2026-okawa <okawa-ryotaro-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-16 10:51:37 +0900

    Update Mamanoyousu.java

--- source/src/main/java/dto/Mamanoyousu.java
+++ source/src/main/java/dto/Mamanoyousu.java
@@ -1,5 +1,11 @@
 package dto;
 
 public class Mamanoyousu {
-
+	private String familyId;	/*家族番号（夫婦共通）*/
+	private int coupleId;		/*夫01、妻00*/
+	private int physicalScore;	/**/
+	private int mentalScore;	/**/
+	private double sleepHours;	/**/
+	private int stress;			/**/
+	private String memo;		/**/
 }
