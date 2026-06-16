a3f53a7da47aec457652bf4063181cb5faea8509
plusdojo2026-okawa <okawa-ryotaro-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-16 15:50:39 +0900

    Update Mamanoyousu.java

--- source/src/main/java/dto/Mamanoyousu.java
+++ source/src/main/java/dto/Mamanoyousu.java
@@ -1,11 +1,71 @@
 package dto;
 
 public class Mamanoyousu {
-	private String familyId;	/*家族番号（夫婦共通）*/
+	private String familyId;	/*家族名（夫婦共通）*/
 	private int coupleId;		/*夫01、妻00*/
-	private int physicalScore;	/**/
-	private int mentalScore;	/**/
-	private double sleepHours;	/**/
-	private int stress;			/**/
-	private String memo;		/**/
+	private int physicalScore;	/*体調*/
+	private int mentalScore;	/*メンタル*/
+	private double sleepHours;	/*睡眠時間*/
+	private int stress;			/*ストレス値*/
+	private String memo;		/*メモ*/
+	
+	public Mamanoyousu() {}
+	
+	public Mamanoyousu(String familyId,int coupleId,int physicalScore,int mentalScore,double sleepHours,
+			int stress, String memo) {
+		this.familyId=familyId;
+		this.coupleId=coupleId;
+		this.physicalScore=physicalScore;
+		this.mentalScore=mentalScore;
+		this.sleepHours=sleepHours;
+		this.stress=stress;
+		this.memo=memo;
+	}
+	
+	//ゲッター・セッター
+	public String getFamilyId(){
+		return familyId;
+	}
+	public void setFamilyId(String familyId) {
+		this.familyId = familyId; }
+	
+	public int getCoupleId() {
+		return coupleId;
+	}
+	public void setCoupleId(int coupleId) {
+		this.coupleId = coupleId;}
+	
+	public int getPhysicalScore() { 
+		return physicalScore; 
+	}
+	public void setPhysicalScore(int physicalScore) { 
+		this.physicalScore = physicalScore;}
+	
+	public int getMentalScore() { 
+		return mentalScore;
+	}
+	public void setMentalScore(int mentalScore) {
+		this.mentalScore = mentalScore;
+	}
+	
+	public double getSleepHours() {
+		return sleepHours;
+	}
+	public void setSleepHours(double sleepHours) {
+		this.sleepHours = sleepHours;
+	}
+	
+	public int getStress() {
+		return stress;
+	}
+	public void setStress(int stress) {
+		this.stress = stress;
+	}
+	
+	public String getMemo() {
+		return memo;
+	}
+	public void setMemo(String memo) {
+		this.memo = memo;
+	}
 }
