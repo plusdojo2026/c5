a8500cc3c736d2f1d9e3953bde46139b42bdb657
plusdojo2026-okawa <okawa-ryotaro-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-17 17:39:16 +0900

    Update SukusukukirokuDao.java

--- source/src/main/java/dao/SukusukukirokuDao.java
+++ source/src/main/java/dao/SukusukukirokuDao.java
@@ -1,5 +1,25 @@
 package dao;
 
-public class SukusukukirokuDao {
+import java.sql.DriverManager;
 
+public class SukusukukirokuDao {
+	//すくすく記録を取得するメソッド
+	public List<Sukusukukiroku> findByFamilyId(String familyId){
+		Connection conn = null;
+		List <sukusukukiroku> list = new AllayList<>();
+		
+			try {
+				//JDBCドライバ
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				
+				//DB接続
+				conn = DriverManager.getConnection(
+						"jdbc:mysql://localhost:3306/",//接続するデータベース名
+			            "root",//MySQLにログインするユーザー名 //⚠root は 管理者ユーザー
+			            ""
+						);	
+				//SQL文
+			}
+	}
+	
 }
