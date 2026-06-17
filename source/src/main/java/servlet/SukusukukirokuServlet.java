e2a08a8177761ff9b29815815021d196077c8189
plusdojo2026-okawa <okawa-ryotaro-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-17 15:13:38 +0900

    Update SukusukukirokuServlet.java

--- source/src/main/java/servlet/SukusukukirokuServlet.java
+++ source/src/main/java/servlet/SukusukukirokuServlet.java
@@ -1,10 +1,90 @@
 package servlet;
 
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.SukusukukirokuDao;
+import dto.Sukusukukiroku;
+
+@WebServlet("/SukusukukirokuServlet")
 public class SukusukukirokuServlet {
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * GETリクエスト：画面表示
+	 *  タイムライン一覧を取得してJSPに渡す
+	 */
+	
+	//画面表示(GET)
+		protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・ス
+			// 文字コードの設定
+	        request.setCharacterEncoding("UTF-8");
+	        
+	        // セッションからfamily_idを取得
+	        HttpSession session = request.getSession();
+	        String familyId = (String) session.getAttribute("family_id");
+	        
+	        // ログインしていない場合はログイン画面に戻す
+	        if (familyId == null) {
+	            response.sendRedirect("login.jsp");
+	            return;
+	        }
+	        
 
-	}
+	            // Daoからタイムラインデータを取得
+	            SukusukukirokuDao dao = new SukusukukirokuDao();
+	            List<Sukusukukiroku> list = dao.findAll();
 
+	           //JSPにデータを渡す 
+	            request.setAttribute("recordList", list);
+
+               // 日付（表示用）
+	            request.setAttribute("now", LocalDateTime.now().toString());
+	            
+	           //JSPに遷移
+	            request.getRequestDispatcher("WEB-INF/jsp/sukusukukiroku.jsp")
+	               .forward(request, response);
+			}	
+		
+				//登録処理
+				protected void doPost(HttpServletRequest request, HttpServletResponse response)
+						throws ServletException, IOException{
+					
+					request.setCharacterEncoding("UTF-8");
+					
+					String t = request.getParameter("temperature");
+					String w = request.getParameter("weight");
+					String note = request.getParameter("note");//任意
+					String message = "";
+					
+					//登録の例外処理
+					try {
+						double temp = Double.parseDouble(t);
+						double weight = Double.parseDouble(w);
+						
+						if (temp < 35|| temp >42) {
+							message = "体温は35~42の間で入力してください";
+						} else if(weight <0|| weight >99) {
+							message ="体重は0~99㎏の間で入力してください";
+						} else {
+							SukusukukirokuDao dao = new SukusukukirokuDao();
+							dao.insert(temp,weight,note);
+							message = "登録完了しました！";
+						}
+					}catch(Exception e) {
+						message = "入力エラー";
+					}
+					
+					doGet(request,response);//再表示
+					request.setAttribute("message", message);
+				}
 }
