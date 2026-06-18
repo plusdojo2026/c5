a449bad32f1482c7795ddcf9916c2b6af58d3212
plusdojo2026-okawa <okawa-ryotaro-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-18 11:58:37 +0900

    Update SukusukukirokuDao.java

--- source/src/main/java/dao/SukusukukirokuDao.java
+++ source/src/main/java/dao/SukusukukirokuDao.java
@@ -1,6 +1,12 @@
 package dao;
 
+import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.List;
+
+import dto.Sukusukukiroku;
 
 public class SukusukukirokuDao {
 	//すくすく記録を取得するメソッド
@@ -19,7 +25,45 @@ public class SukusukukirokuDao {
 			            ""
 						);	
 				//SQL文
+				String sql ="""
+						SELECT*
+						FROM sukusukukiroku
+						WERER family_id = ?
+						ORDER BY recorded_at DESC
+						""";
+
+				PreparedStatement pStmt = conn.prepareStatement(sql);
+	            pStmt.setString(1, familyId);
+
+	            // SQL文を実行し結果を取得
+	            ResultSet rs = pStmt.executeQuery();
+	            
+	            //DTOに詰める
+	            while(rs.next()) {
+	            	Sukusukukiroku record =new Sukusukukiroku();
+	            	
+	            	record.setSukusukuId(rs.getInt("sukusuku_id"));
+	            	record.setFamilyId(rs.getString("family_id"));
+	            	record.setWeight(rs.getDouble("weight"));
+	            	record.setTemperature(rs.getDouble("temperature"));
+	            	record.setNote(rs.getString("note"));
+	            	record.setRecordedAt(rs.getString("recorded_at"));
+	            	
+	            	list.sdd(record);
+	            }
+	            
+			}catch (Exception e) {
+				e.printStackTrace();
+			}finally{
+				//DB切断
+				try {
+					if (conn!= null)conn.close();
+				}catch(Exception e) {
+					e.printStackTrace();
+				}
 			}
+			return list;
 	}
+	//最新データ取得
 	
 }
