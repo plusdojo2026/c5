2852823aa0432fad2fd2b7de907aaf5a50419991
plusdojo2026-mukogawa <mukogawa-ryo-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-19 17:40:21 +0900

    Create omoidealbum.sql

---
+++ source/sql/omoidealbum.sql
@@ -0,0 +1,26 @@
+--候補1
+CREATE TABLE album(
+
+	omoidealbum_id INT AUTO_INCREMENT PRIMARY KEY,
+	
+	family_id VARCHAR(32) NOT NULL UNIQUE,
+	album_id INT NOT NULL,
+	                          
+    couple_id INT NOT NULL,                          
+    photo_path VARCHAR(255) NOT NULL,                
+    comment VARCHAR(200) NOT NULL,                   
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    
+);
+
+--候補2
+CREATE TABLE album(
+
+	album_id INT AUTO_INCREMENT PRIMARY KEY,
+	family_id VARCHAR(32) NOT NULL UNIQUE,
+	                          
+    couple_id INT NOT NULL,                          
+    photo_path VARCHAR(255) NOT NULL,                
+    comment VARCHAR(200) NOT NULL,                   
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
