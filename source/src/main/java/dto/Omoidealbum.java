c0f3d83d85c5113c1655c3f3e85061dea1620c84
plusdojo2026-mukogawa <mukogawa-ryo-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-22 09:48:37 +0900

    Update Omoidealbum.java

--- source/src/main/java/dto/Omoidealbum.java
+++ source/src/main/java/dto/Omoidealbum.java
@@ -6,6 +6,7 @@ import java.sql.Timestamp;
 public class Omoidealbum implements Serializable {
 	private String familyId;		/*ファミリーID*/
 	private int coupleId;			/*カップルID　妻=0,夫=1*/
+	/*private int familyalbumId;		ファミリーアルバムID*/
 	private int albumId;			/*自動採番*/
 	private String photoPath;		/*写真の保存パス*/
 	private String comment;			/*コメント*/
@@ -16,7 +17,7 @@ public class Omoidealbum implements Serializable {
 		super();
 		this.familyId = familyId;
 		this.coupleId = coupleId;
-		this.albumId = albumId;
+		/*this.familyalbumId = familyalbumId;*/
 		this.photoPath = photoPath;
 		this.comment = comment;
 		this.createdAt = createdAt;
@@ -26,7 +27,8 @@ public class Omoidealbum implements Serializable {
 	public Omoidealbum() {
 		super();
 		this.familyId = "";
-		this.coupleId = -1; //0=妻のため、-1で未設定を表す
+		this.coupleId = -1; /*0=妻のため、-1で未設定を表す*/
+		/*this.familyalbumId = 0;*/
 		this.albumId = 0;
 		this.photoPath = "" ;
 		this.comment = "" ;
@@ -49,6 +51,14 @@ public class Omoidealbum implements Serializable {
 	public void setCoupleId(int coupleId) {
 		this.coupleId = coupleId;
 	}
+	
+	/*public int getfamilyalbumId() {
+		return familyalbumId;
+	}
+
+	public void setfamilyalbumId(int familyalbumId) {
+		this.familyalbumId = familyalbumId;
+	}*/
 	
 	public int getAlbumId() {
 		return albumId;
